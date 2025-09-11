d9280d7c2d6ee0cbdf50d79d397b433605895030
eugsa <eugenie.saliou@gmail.com>
2025-09-11 16:11:34 +0200

    Modify next.config

--- next.config.js
+++
@@ -1,9 +0,0 @@
-// @ts-check
- 
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  basePath: "/vitrine",
-  reactStrictMode: true,
-}
- 
-module.exports = nextConfig
---
+++ next.config.ts
@@ -0,0 +1,11 @@
+import type { NextConfig } from "next";
+
+const nextConfig: NextConfig = {
+  basePath: "/vitrine",
+  reactStrictMode: true,
+  output: "export", // enables static exports
+  images: {
+    unoptimized: true, // disables server-based image optimization.
+  }
+}
+module.exports = nextConfig
